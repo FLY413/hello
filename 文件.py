6c049a33e582303681c4a760eb697f8b16400a4a
FLY413 <1572549253@qq.com>
2025-03-22 23:30:59 +0800

    python
    python

---
+++ 文件.py
@@ -0,0 +1,7 @@
+print('hello')
+print('world')
+print('hello', 'world')
+print('hello', 'world', sep='***')
+print('hello', 'world', end='***')  # end=' ' 末尾に改行文字を追加する
+print('hello', 'world', end='***\n')                # end=' ' 末尾に改行文字を追加する
+print('hello', 'world', end='***\n')                # end=' ' 末尾に改行文字を追加する
